fee980768bbe612bd0a13ef25a6fd41e2e79bbd0
salvatorebriga <salvo.z2302@gmail.com>
2024-05-29 19:18:20 +0200

    added AppHero with style

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,24 @@
 <script>
   import AppTitle from "./components/AppTitle.vue";
+  import AppHero from "./components/AppHero.vue";
   export default {
     data() {
       return {};
     },
     components: {
       AppTitle,
+      AppHero,
     },
   };
 </script>
 
 <template>
   <AppTitle />
+  <AppHero />
 </template>
 
-<style scoped></style>
+<style>
+  div {
+    text-align: center;
+  }
+</style>
---
+++ src/components/AppHero.vue
@@ -0,0 +1,23 @@
+<script>
+  export default {
+    name: "AppHero",
+    data() {
+      return {
+        Text: "Sono il JumboTron!",
+      };
+    },
+  };
+</script>
+<template>
+  <div>
+    <h1>{{ Text }}</h1>
+  </div>
+</template>
+<style scoped>
+  div {
+    width: 100%;
+    padding-top: 150px;
+    padding-bottom: 150px;
+    background-color: aquamarine;
+  }
+</style>
