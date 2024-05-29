f38c9802f1aff83693d2fd010440481f11de3e8b
salvatorebriga <salvo.z2302@gmail.com>
2024-05-29 19:09:53 +0200

    added component

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,21 @@
+<script>
+  export default {
+    name: "AppTitle",
+    data() {
+      return {
+        Text: "La mia prima app con Vite!",
+      };
+    },
+  };
+</script>
+<template>
+  <div>
+    <h1>{{ Text }}</h1>
+  </div>
+</template>
+<style>
+  div {
+    padding: 25px;
+    text-align: center;
+  }
+</style>
